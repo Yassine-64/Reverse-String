f9a90a0321db0aaf1f4a5fd256181ccc8cfe8092
Yassine mazhare <137923652+Yassine-64@users.noreply.github.com>
2023-10-18 14:04:11 +0200

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,5 @@
+chaine = input("entrez une chaine de caractere:")
+chaine_list = list(chaine)
+chaine_list.reverse()
+chaine_renver = ''.join(chaine_list)
+print("la chaine renversee est :", chaine_renver)
